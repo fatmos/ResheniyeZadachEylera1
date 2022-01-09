a763b19b15f370682fb7ea34fd4f72cebb0e8034
fatmos <58290898+fatmos@users.noreply.github.com>
2022-01-10 02:09:52 +0300

    Add files via upload

---
+++ zadacha5.kt
@@ -0,0 +1,27 @@
+
+fun main(){
+    var n=20
+    var i=19
+    while (i > 0){
+        if (n % i ==0){
+            i-=1
+        }
+
+        else{
+            n+=20
+            i=19
+        }
+
+        if (i==1) {
+            print(n)
+        }
+    }
+
+
+
+
+}
+
+
+
+
